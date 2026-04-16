69fb21356d7ffd01617ccaaf9e00a5521a2d29e4
tommyxlanes <tommyxlanes@gmail.com>
2026-04-15 19:28:44 -0700

    Updated removed dark mode in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,6 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
-  darkMode: "class",
   theme: {
     extend: {
       colors: {
